ad98e7d01725b30e80e839cee78ca806cbb6c074
Felicity Brammer <felicitybrammer@gmail.com>
2022-01-26 16:40:42 -0700

    fixes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -15,6 +17,22 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
+
+// const startServer = async () => {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: authMiddleware
+//   });
+
+//   await server.start();
+//   server.applyMiddleware({ app });
+//   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+// };
+
+// startServer()
+
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
